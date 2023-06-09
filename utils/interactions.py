83baff34c2976acf8435646036eb8eeea6fbe4ef
unknown <zenitica2030@gmail.com>
2023-06-09 15:39:19 +0800

    dev_2: marks utils/interactions.py, as gravity calculations integrated in utils/object.py. May be used later

--- utils/interactions.py
+++ utils/interactions.py
@@ -2,8 +2,8 @@ from physical_quantity import G
 from objects import UniverseObject
 
 
-def calculate_gravity_force(obj1, obj2):
-    delta_displacement = obj2.displacement.vector_subtract(obj1.displacement)
-    dist = delta_displacement.vector_l2_norm()
-    force_magnitude = G * obj1.mass * obj2.mass / dist
-    return force_magnitude
+# def calculate_gravity_force(obj1, obj2):
+#     delta_displacement = obj2.displacement.vector_subtract(obj1.displacement)
+#     dist = delta_displacement.vector_l2_norm()
+#     force_magnitude = G * obj1.mass * obj2.mass / dist
+#     return force_magnitude
